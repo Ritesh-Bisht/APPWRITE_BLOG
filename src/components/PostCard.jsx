34b16fd60a798c4b4169822511c1611107aab80f
RITESH BISHT <141986335+Ritesh-Bisht@users.noreply.github.com>
2024-07-12 13:38:18 +0530

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import appwriteService from "../appwrite/config"
-import {Link} from 'react-router-dom'
+import React from 'react';
+import appwriteService from "../appwrite/config";
+import { Link } from 'react-router-dom';
 
-function PostCard({$id, title, featuredImage}) {
-    
+function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
-
-            </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
+      <div className='w-full bg-gray-100 rounded-xl p-4 h-64 flex flex-col'>
+        <div className='w-full justify-center mb-4 flex-grow overflow-hidden'>
+          <img 
+            src={appwriteService.getFilePreview(featuredImage)} 
+            alt={title}
+            className='rounded-xl w-full h-32 object-contain' 
+          />
         </div>
+        <h2 className='text-xl font-bold overflow-hidden text-ellipsis'>
+          {title}
+        </h2>
+      </div>
     </Link>
-  )
+  );
 }
 
+export default PostCard;
 
-export default PostCard
